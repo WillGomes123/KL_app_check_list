8f1dce99fe149c9e34d6d989696b898a7d00e733
William Gomes <williamdossantosgomes.gomes@gmail.com>
2025-05-13 17:38:56 -0300

    teste-chamada

--- src/app/kl/checklist/checklist.page.ts
+++ src/app/kl/checklist/checklist.page.ts
@@ -386,7 +386,7 @@ export class ChecklistPage {
           observacao:'Cliente liberado, sem avarias'
 
         };
-
+// teste api libercao
         this.apiKlService.liberacaoLocacao(dadosLiberacao).subscribe(
           res=>{
             if(res.status === true){
